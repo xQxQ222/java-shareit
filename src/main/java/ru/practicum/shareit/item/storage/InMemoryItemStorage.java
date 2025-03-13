1a8df2c0186544ea5dde53f6aeb02b56fa9c74c5
Ivan Korolkov <nerounxqxq@gmail.com>
2025-03-14 00:11:23 +0400

    Fix code style 2

--- src/main/java/ru/practicum/shareit/item/storage/InMemoryItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/InMemoryItemStorage.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.storage;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.exceptions.NotFoundException;
 import ru.practicum.shareit.exception.exceptions.NotOwnerException;
-import ru.practicum.shareit.exception.exceptions.ValidationException;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
